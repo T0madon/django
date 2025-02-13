b4d945ee591695fba4cdcbb8e7eb95f37b016686
T0madon <joao.daciuk@gmail.com>
2025-02-12 23:10:34 -0300

    views

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def blog(request):
     print('blog')
-    return HttpResponse('blog do app')
+    return render(
+        request,
+        'blog/index.html'
+    )
+
+def exemplo(request):
+    print('exemplo')
+    return render(
+        request,
+        'blog/exemplo.html'
+    )
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
     print('home')
-    return HttpResponse('home do app')
+    
+    return render(
+        request,
+        'home/index.html'
+    )
