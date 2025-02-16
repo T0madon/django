aac39e503162dc115f5a5162903ff53759a77029
T0madon <joao.daciuk@gmail.com>
2025-02-16 11:42:38 -0300

    usando context

--- blog/views.py
+++ blog/views.py
@@ -2,14 +2,28 @@ from django.shortcuts import render
 
 def blog(request):
     print('blog')
+
+    context = {
+            'text' : 'Olá blog',
+            'title' : 'Blog - '
+        }
+
     return render(
         request,
-        'blog/index.html'
+        'blog/index.html',
+        context
     )
 
 def exemplo(request):
     print('exemplo')
+
+    context = {
+            'text' : 'Olá exemplo',
+            'title' : 'Exemplo - '
+        }
+
     return render(
         request,
-        'blog/exemplo.html'
+        'blog/exemplo.html',
+        context
     )
--- home/views.py
+++ home/views.py
@@ -2,8 +2,13 @@ from django.shortcuts import render
 
 def home(request):
     print('home')
+
+    context = {
+            'text' : 'Estamos na HOME'
+        }
     
     return render(
         request,
-        'home/index.html'
+        'home/index.html',
+        context
     )
