7527c745ec183d67772bc5910d31364413814e13
T0madon <joao.daciuk@gmail.com>
2025-02-19 19:40:56 -0300

    style e adicionando logica de id

--- blog/views.py
+++ blog/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from blog.data import posts
 
 def blog(request):
-    print('blog')
 
     context = {
             'posts' : posts
@@ -14,17 +13,23 @@ def blog(request):
         context
     )
 
-def post(request, id):
-    print('post', id)
+def post(request, post_id):
+    found_post = None
+
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+
 
     context = {
-            # 'text' : 'Olá blog',
-            'posts' : posts
+            'post' : found_post,
+            'title' : found_post['title'] + ' - '
         }
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post.html',
         context
     )
 
